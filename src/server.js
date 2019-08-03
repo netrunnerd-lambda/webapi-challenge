71b0ed28b8d539a4d634fd02e47efa83b38e26ad
netrunnerd <41193007+netrunnerd@users.noreply.github.com>
2019-08-02 21:09:45 -0400

    Add a basic server and make way for routes

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json({ 
+    message: "new phone. who dis?",
+    success: true
+  });
+});
+
+module.exports = server;
